8dfabc944e0878aeea9084ea536e4baef5d02a94
Sanjay Sarmah <ssharma.asm@gmail.com>
2023-09-08 00:57:00 +0530

    implementing Pandas

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-x = [1,3,5,7,9]
-y = [2,4,6,8,10]
+import pandas as pd
+c = pd.read_csv('covid_19_india.csv')
 
-print(type(x))
-print(type(y))
-
-import matplotlib.pyplot as plt
-plt.plot(x,y,marker=10, c='yellow', s=100)
-plt.xlabel('Population')
-plt.ylabel('GDP per capita')
-
-plt.show()
