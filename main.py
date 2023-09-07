50e2a8ae5f84ecdf30429b938dbbdd4271e0a046
Sanjay Sarmah <ssharma.asm@gmail.com>
2023-09-08 01:11:25 +0530

    implementing Pandas

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pandas as pd
-c = pd.read_csv('covid_19_india.csv')
+import matplotlib.pyplot as plt
+data = pd.read_csv('covid_19_india.csv')
+data.head()
+print(data)
+
+plt.scatter(data.Deaths, data.Confirmed, s=150, c=data.Confirmed,marker=10 )
+plt.grid(100)
+plt.title("Covid Mortaility")
+plt.xlabel("Number of Death")
+plt.ylabel("Deaths confirmed")
+
+
+plt.show()
 
